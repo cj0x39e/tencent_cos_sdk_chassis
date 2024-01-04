21ef42d729f013f6ffec30522a3d59fcae7c1c7d
cj0x39e <taozhi1028@gmail.com>
2024-01-04 13:08:40 +0800

    add sign

--- lib/src/sign.dart
+++ lib/src/sign.dart
@@ -1,14 +1,133 @@
-const defaultExpiredTime = 2 * 60 * 60 * 1000;
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
+import 'package:tencent_cos_plus/src/utils.dart';
+
+const validHeaders = {
+  "cache-control",
+  "content-disposition",
+  "content-encoding",
+  "content-type",
+  "expires",
+  "content-md5",
+  "content-length",
+  "host"
+};
 
 class Sign {
-  /// [expiredTime] in milliseconds with default value is 2 hours
-  final int expiredTime;
+  final int startTimeMs;
 
-  Sign({this.expiredTime = defaultExpiredTime});
+  final int expiredTimeMs;
 
-  generateKeyTime() {
-    final startTimestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000 - 60;
-    final endTimestamp = expiredTime ~/ 1000;
+  final String secretKey;
+
+  final String secretId;
+
+  final String method;
+
+  final String uriPathname;
+
+  final Map<String, String>? params;
+
+  final Map<String, String>? headers;
+
+  Sign({
+    required this.expiredTimeMs,
+    required this.startTimeMs,
+    required this.secretKey,
+    required this.secretId,
+    required this.method,
+    required this.uriPathname,
+    this.params,
+    this.headers,
+  })  : assert(expiredTimeMs > startTimeMs),
+        assert(secretKey.isNotEmpty),
+        assert(secretId.isNotEmpty),
+        assert(method.isNotEmpty),
+        assert(uriPathname.isNotEmpty);
+
+  String getSignature() {
+    final keyTime = generateKeyTime(startTimeMs, expiredTimeMs);
+    final (paramList: urlParamList, parameters: httpParameters) =
+        generateParamsListAndParameters(params ?? {});
+    final (paramList: headerList, parameters: httpHeaders) =
+        generateParamsListAndParameters(filterHeaders(headers ?? {}));
+    final httpString = generateHttpString(
+        method: method,
+        uriPathname: uriPathname,
+        httpParameters: httpParameters,
+        httpHeaders: httpHeaders);
+    final stringToSign = generateStringToSign(keyTime, httpString);
+    final signature = generateSignature(stringToSign, secretKey);
+
+    return [
+      'q-sign-algorithm=sha1',
+      'q-ak=$secretId',
+      'q-sign-time=$keyTime',
+      'q-key-time=$keyTime',
+      'q-header-list=$headerList',
+      'q-url-param-list=$urlParamList',
+      'q-signature=$signature'
+    ].join('&');
+  }
+
+  static String generateKeyTime(int startTimeMs, int expiredTimeMs) {
+    assert(expiredTimeMs > startTimeMs);
+
+    final startTimestamp = startTimeMs ~/ 1000;
+    final endTimestamp = expiredTimeMs ~/ 1000;
+
     return '$startTimestamp;$endTimestamp';
+  }
+
+  static String generateSignKey(String keyTime, String secretKey) {
+    return Utils.HMACSha1(keyTime, secretKey);
+  }
+
+  static ({String paramList, String parameters})
+      generateParamsListAndParameters(Map<String, String> params) {
+    final paramsEncoded = params.map((key, value) => MapEntry(
+        Uri.encodeComponent(key).toLowerCase(), Uri.encodeComponent(value)));
+
+    final keys = paramsEncoded.keys.toList()..sort();
+
+    final paramList = keys.join(";");
+    final parameters =
+        keys.map((key) => '$key=${paramsEncoded[key]}').join("&");
+
+    return (paramList: paramList, parameters: parameters);
+  }
+
+  static String generateHttpString({
+    required String method,
+    required String uriPathname,
+    required String httpParameters,
+    required String httpHeaders,
+  }) {
+    return '${method.toLowerCase()}\n$uriPathname\n$httpParameters\n$httpHeaders\n';
+  }
+
+  static String generateStringToSign(String keyTime, String httpString) {
+    return "sha1\n$keyTime\n${hex.encode(sha1.convert(httpString.codeUnits).bytes)}\n";
+  }
+
+  static generateSignature(String stringToSign, String secretKey) {
+    return Utils.HMACSha1(stringToSign, secretKey);
+  }
+
+  static filterHeaders(Map<String, String> headers) {
+    Map<String, String> res = {};
+
+    for (final key in headers.keys) {
+      if (validHeaders.contains(key.toLowerCase()) ||
+          key.toLowerCase().startsWith("x")) {
+        if (key == "content-length" && headers["content-length"] == "0") {
+          continue;
+        }
+
+        res[key] = headers[key] ?? '';
+      }
+    }
+
+    return res;
   }
 }
--- test/sign_test.dart
+++ test/sign_test.dart
@@ -2,8 +2,134 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tencent_cos_plus/src/sign.dart';
 
 void main() {
-  test('running generateKeyTime with default expired time', () {
-    final sign = Sign();
-    expect(sign.generateKeyTime(), '1');
+  late int startTimeMs;
+  late int expiredTimeMs;
+  late String secretKey;
+  late String secretId;
+  late String method;
+  late String uriPathname;
+  late Map<String, String> params;
+  late Map<String, String> headers;
+  late String keyTime;
+  late String httpString;
+  late String stringToSign;
+  late String httpHeaders;
+  late String httpParameters;
+
+  setUp(() {
+    startTimeMs = DateTime(2023, 1, 1).millisecondsSinceEpoch;
+    expiredTimeMs = startTimeMs + 2 * 60 * 60 * 1000;
+    secretKey = 'secretKey';
+    secretId = 'secretId';
+    params = {'Name': 'cj', 'Age': '8', 'date': '2023-02-02'};
+    headers = {'AUTH': '', 'Content-Type': 'application/json'};
+    method = 'POST';
+    uriPathname = 'jekfjig';
+
+    keyTime = Sign.generateKeyTime(startTimeMs, expiredTimeMs);
+    (paramList: _, parameters: httpHeaders) =
+        Sign.generateParamsListAndParameters(Sign.filterHeaders(headers));
+    (paramList: _, parameters: httpParameters) =
+        Sign.generateParamsListAndParameters(params);
+    httpString = Sign.generateHttpString(
+        method: method,
+        uriPathname: uriPathname,
+        httpParameters: httpParameters,
+        httpHeaders: httpHeaders);
+    stringToSign = Sign.generateStringToSign(keyTime, httpString);
+  });
+
+  test('generate key time', () {
+    final keyTime = Sign.generateKeyTime(startTimeMs, expiredTimeMs);
+
+    expect(keyTime, '${startTimeMs ~/ 1000};${expiredTimeMs ~/ 1000}');
+  });
+
+  test(
+      'generate key time should throw an exception when expired time is less than start time',
+      () {
+    final now = DateTime.now().millisecondsSinceEpoch;
+    final end = now - 2 * 60 * 60 * 1000;
+
+    try {
+      Sign.generateKeyTime(now, end);
+    } catch (e) {
+      expect(e, isAssertionError);
+    }
+  });
+
+  test('generate sign key', () {
+    final signKey = Sign.generateSignKey(keyTime, secretKey);
+    expect(signKey, 'b6b74c69ecf10928a0d2a00a4ef3f72703743251');
+  });
+
+  test('generate params keys and values with empty params', () {
+    final (paramList: list, parameters: params) =
+        Sign.generateParamsListAndParameters({});
+
+    expect(list, '');
+    expect(params, '');
+  });
+
+  test('generate params keys and values', () {
+    final (paramList: list, parameters: paramsStr) =
+        Sign.generateParamsListAndParameters(params);
+
+    expect(list, 'age;date;name');
+    expect(paramsStr, 'age=8&date=2023-02-02&name=cj');
+  });
+
+  test('generate http string', () {
+    final result = Sign.generateHttpString(
+        method: method,
+        uriPathname: uriPathname,
+        httpHeaders: httpHeaders,
+        httpParameters: httpParameters);
+
+    expect(result,
+        'post\njekfjig\nage=8&date=2023-02-02&name=cj\ncontent-type=application%2Fjson\n');
+  });
+
+  test('generate string to sign', () {
+    final result = Sign.generateStringToSign(keyTime, httpString);
+
+    expect(result,
+        'sha1\n1672502400;1672509600\n2b8209cd0ee3ea73800ede818422de6805a1910b\n');
+  });
+
+  test('generate signature', () {
+    final result = Sign.generateSignature(stringToSign, secretKey);
+
+    expect(result, 'f033a37930d1e70ddf942e987cf3899bbb3716df');
+  });
+
+  test('get signature without headers and params', () {
+    final result = Sign(
+      startTimeMs: startTimeMs,
+      expiredTimeMs: expiredTimeMs,
+      secretKey: secretKey,
+      secretId: secretId,
+      method: method,
+      uriPathname: uriPathname,
+    ).getSignature();
+
+    expect(result,
+        'q-sign-algorithm=sha1&q-ak=secretId&q-sign-time=1672502400;1672509600&q-key-time=1672502400;1672509600&q-header-list=&q-url-param-list=&q-signature=20b27c57d4d0eaa534d3370b972e69cd01bd569f');
+  });
+
+  test('get signature with headers and params', () {
+    final result = Sign(
+      startTimeMs: startTimeMs,
+      expiredTimeMs: expiredTimeMs,
+      secretKey: secretKey,
+      secretId: secretId,
+      method: method,
+      uriPathname: uriPathname,
+      headers: headers,
+      params: params,
+    ).getSignature();
+
+    expect(result,
+        'q-sign-algorithm=sha1&q-ak=secretId&q-sign-time=1672502400;1672509600&q-key-time=1672502400;1672509600&q-header-list=content-type&q-url-param-list=age;date;name&q-signature=f033a37930d1e70ddf942e987cf3899bbb3716df');
   });
 }

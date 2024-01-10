597bc297d626b2b4a6f2f0934cec01ee89e33426
cj0x39e <taozhi1028@gmail.com>
2024-01-10 15:09:55 +0800

    Supports retrieving the current progress when using 'putObject'

--- lib/apis/put_object.dart
+++ lib/apis/put_object.dart
@@ -12,6 +12,7 @@ extension COSPutObject on COSClient {
     required String filePath,
     required String bucket,
     required String key,
+    void Function(int uploadedBytes, int totalBytes)? progress,
     String? region,
     Map<String, String>? headers,
     Map<String, String>? params,
@@ -29,24 +30,41 @@ extension COSPutObject on COSClient {
           (COSFetchContext context) async {
             final req = context.req;
 
-            final mime = lookupMimeType(filePath);
-            final file = File(filePath);
-            final fileLength = file.lengthSync();
-            final fs = file.openRead();
+            if (req != null) {
+              final mime = lookupMimeType(filePath);
+              final file = File(filePath);
+              final fileLength = file.lengthSync();
+              final fs = file.openRead();
+              int uploadedBytes = 0;
 
-            req?.headers
-                .add('Content-Type', mime ?? 'application/octet-stream');
-            req?.headers.add('Content-Length', fileLength);
+              req.headers
+                  .add('Content-Type', mime ?? 'application/octet-stream');
+              req.headers.add('Content-Length', fileLength);
 
-            await req?.addStream(fs);
+              await req.addStream(fs.map((size) {
+                uploadedBytes += size.length;
+
+                if (progress != null) {
+                  progress(uploadedBytes, fileLength);
+                }
+
+                return size;
+              }));
+            } else {
+              throw COSException(message: 'req is null');
+            }
           }
         ],
         resHandlers: [
           (fetchContext, data) async {
             final res = fetchContext.res;
 
-            if (res?.statusCode != HttpStatus.ok) {
-              throw COSException(res: res);
+            if (res != null) {
+              if (res.statusCode != HttpStatus.ok) {
+                throw COSException(res: res);
+              }
+            } else {
+              throw COSException(message: 'res is null');
             }
 
             COSLogger.t('putObject: end');

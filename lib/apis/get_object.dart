0e558e22dd5fe88cecef661cabfe8e7d909f1dbe
cj0x39e <taozhi1028@gmail.com>
2024-01-10 14:40:44 +0800

    Supports retrieving the current progress when using 'getObject'

--- lib/apis/get_object.dart
+++ lib/apis/get_object.dart
@@ -7,10 +7,12 @@ extension COSGutObject on COSClient {
   /// 下载对象
   /// 参考文档： https://cloud.tencent.com/document/product/436/7749
   /// [savePath] 文件下载路径
+  /// [progress] 下载进度
   Future<File?> getObject({
     required String savePath,
     required String bucket,
     required String key,
+    void Function(int downloadedBytes, int totalBytes)? progress,
     String? region,
     Map<String, String>? headers,
     Map<String, String>? params,
@@ -28,18 +30,42 @@ extension COSGutObject on COSClient {
           (fetchContext, data) async {
             final res = fetchContext.res;
 
-            File? file;
+            if (res != null) {
+              if (res.statusCode == HttpStatus.ok) {
+                final totalBytes = res.contentLength;
+                int downloadedBytes = 0;
 
-            if (res?.statusCode == HttpStatus.ok) {
-              file = File(savePath);
-              await res?.pipe(file.openWrite());
+                COSLogger.t('getObject: totalBytes: $totalBytes');
+                COSLogger.t(
+                    'getObject: contentType: ${res.headers.contentType}');
+
+                File file = File(savePath);
+                final sink = file.openWrite();
+
+                await res.map((chunk) {
+                  downloadedBytes += chunk.length;
+
+                  COSLogger.t('$downloadedBytes / $totalBytes');
+
+                  if (progress != null) {
+                    progress(downloadedBytes, totalBytes);
+                  }
+
+                  return chunk;
+                }).pipe(sink);
+
+                return file;
+              } else {
+                throw COSException(res: res);
+              }
             } else {
-              throw COSException(res: res);
+              throw Exception('getObject: res is null');
             }
-
+          },
+          (context, data) async {
             COSLogger.t('getObject: end');
 
-            return file;
+            return Future.value(data);
           }
         ]));
   }
